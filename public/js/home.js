dfa83e2adf483192cc60473d7bb2d183913c6820
edenilson <edenilsonsouza.bsi@gmail.com>
2025-11-04 10:44:59 -0300

    Documenting lines of code

--- public/js/home.js
+++ public/js/home.js
@@ -1,12 +1,22 @@
+// HOME JS
+
 const myModal = new bootstrap.Modal("#transaction-modal");
 let logged = sessionStorage.getItem("logged");
 const session = localStorage.getItem("session");
 
+// Declara variáveis globais
 let data = {
     transactions: []
 };
 
+// ============================
+// EVENTOS
+// ============================
+
+// Botão de logout
 document.getElementById("button-logout").addEventListener("click", logout);
+
+//EXIBE AS TRANSAÇÕES
 document.getElementById("transactions-button").addEventListener("click", function (){
     window.location.href = "transactions.html"
 })
@@ -15,72 +25,98 @@ document.getElementById("transactions-button").addEventListener("click", functio
 document.getElementById("transaction-form").addEventListener("submit", function(e) {
     e.preventDefault();
 
+    // Pega os dados do formulário
     const value = parseFloat(document.getElementById("value-input").value);
     const description = document.getElementById("description-input").value;
     const date = document.getElementById("date-input").value;
+
+    // Pega o tipo de lançamento
     const type = document.querySelector('input[name="type-input"]:checked').value;
 
+    // Adiciona o novo lançamento ao array existente
     data.transactions.unshift({
         value: value, type: type, description: description, date: date
     });
 
+    // Salva os dados
     saveData(data);
     e.target.reset();
     myModal.hide();
 
+    // Atualiza a interface
     getCashIn();
     getCashOut();
     getTotal();
 
+    // Exibe mensagem de sucesso
     alert("Lançamento adicionado com sucesso.");
     
 });
 
+// ============================
+// FUNÇÕES
+// ============================
+
+// Verifica se o usuário está logado
 checkLogged();
 
 function checkLogged() {
+  // Verifica a sessão
     if(session) {
         sessionStorage.setItem("logged", session);
         logged = session;
     }
 
+    // Redireciona se não estiver logado
     if(!logged) {
         window.location.href = "index.html";
         return;
     }
 
+    // Pega os dados do usuário
     const dataUser = localStorage.getItem(logged);
     if(dataUser) {
         data = JSON.parse(dataUser);
     }
 
+    // Atualiza a interface
     getCashIn();
     getCashOut();
     getTotal();
 }
 
+// Deslogar usuário
 function logout() {
     sessionStorage.removeItem("logged");
     localStorage.removeItem("session");
 
+    // Redireciona para a página de login
     window.location.href = "index.html";
 }
 
+// ============================
+// EXIBE AS TRANSAÇÕES
+// ============================
 function getCashIn() {
+  // Pega as transações de entrada
     const transactions = data.transactions;
 
+    // Filtra apenas as transações de entrada
     const cashIn = transactions.filter((item) => item.type === "1");
 
+    // Exibe as transações na interface
     if(cashIn.length) {
         let cashInHtml = ``;
         let limit = 0;
 
+        // Define o limite de exibição
         if(cashIn.length > 5) {
             limit = 5;
         } else {
             limit = cashIn.length;
         }
 
+        // Loop pelas transações
         for (let index = 0; index < limit; index++) {
             cashInHtml += `
             <div class="row mb-4">
@@ -101,25 +137,34 @@ function getCashIn() {
             `
         }
 
+        // Exibe o HTML na interface
         document.getElementById("cash-in-list").innerHTML = cashInHtml;
     }
 }
 
+// ============================
+// EXIBE AS SAÍDAS
+// ============================
 function getCashOut() {
+  // Pega as transações de saída
     const transactions = data.transactions;
 
+    // Filtra apenas as transações de saída
     const cashIn = transactions.filter((item) => item.type === "2");
 
+    // Exibe as transações na interface
     if(cashIn.length) {
         let cashInHtml = ``;
         let limit = 0;
 
+        // Define o limite de exibição
         if(cashIn.length > 5) {
             limit = 5;
         } else {
             limit = cashIn.length;
         }
 
+        // Loop pelas transações
         for (let index = 0; index < limit; index++) {
             cashInHtml += `
             <div class="row mb-4">
@@ -140,14 +185,20 @@ function getCashOut() {
             `
         }
 
+        // Exibe o HTML na interface
         document.getElementById("cash-out-list").innerHTML = cashInHtml;
     }
 }
 
+// ============================
+// EXIBE O TOTAL
+// ============================
 function getTotal() {
+  // Calcula o total das transações
     const transactions = data.transactions;
     let total = 0;
 
+    // Loop pelas transações
     transactions.forEach((item) => {
         if(item.type === "1") {
             total += item.value;
@@ -156,9 +207,14 @@ function getTotal() {
         }
     });
 
+    // Exibe o total na interface
     document.getElementById("total").innerHTML = `R$ ${total.toFixed(2)}`;
 }
 
+// ============================
+// SALVA OS DADOS
+// ============================
 function saveData(data) {
+  // Salva os dados no localStorage
     localStorage.setItem(data.login, JSON.stringify(data));
 }
--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,6 @@
+// ============================
+// VARIÁVEIS
+// ============================
 const myModal = new bootstrap.Modal("#register-modal");
 let logged = sessionStorage.getItem("logged");
 const session = localStorage.getItem("session");
@@ -14,19 +17,24 @@ document.getElementById("login-form").addEventListener("submit", function(e) {
 
     const account = getAccount(email);
 
+    // Verifica se a conta existe
     if(!account) {
         alert("Ops! Verifique o usuário ou a senha.")
         return;
     }
 
+    // Verifica se a senha está correta
     if(account) {
+      // Verifica se a senha está correta
         if(account.password !== password) {
             alert("Ops! Verifique o usuário ou a senha.");
             return;
         }
 
+        // Salva a sessão
         saveSession(email, checkSession);
 
+        // Redireciona para a página home
         window.location.href = "home.html";
     }
 });
@@ -35,36 +43,52 @@ document.getElementById("login-form").addEventListener("submit", function(e) {
 document.getElementById("create-form").addEventListener("submit", function(e) {
     e.preventDefault();
 
+    // Pega os dados do formulário
     const email = document.getElementById("email-create-input").value;
+  
     const password = document.getElementById("password-create-input").value;
 
+    // Validações
+
+    // E-mail com no mínimo 5 caracteres
     if(email.length < 5) {
         alert("Preencha o campo com um e-mail válido.");
         return;
     }
 
+    // Senha com no mínimo 4 dígitos
     if(password.length < 4) {
         alert("Preencha a senha com no mínimo 4 dígitos.")
         return;
     }
 
+    // Salva a conta
     saveAccount({
         login: email,
         password: password,
         transactions: []
     })
 
+    // Fecha o modal
     myModal.hide();
 
+    // Exibe mensagem de sucesso
     alert("Conta criada com sucesso.");
 });
 
+// ============================
+// FUNÇÕES
+// ============================
+
+// Verifica se o usuário está logado
 function checkLogged() {
+  // Verifica a sessão
     if(session) {
         sessionStorage.setItem("logged", session);
         logged = session;
     }
 
+    // Redireciona se não estiver logado
     if(logged) {
         saveSession(logged, session);
 
@@ -72,11 +96,19 @@ function checkLogged() {
     }
 }
 
+// ============================
+// SALVA A CONTA
+// ============================
+
+// Salva os dados da conta no localStorage
 function saveAccount(data) {
+  // Salva os dados da conta no localStorage
     localStorage.setItem(data.login, JSON.stringify(data));
 }
 
+// Salva a sessão do usuário
 function saveSession(data, saveSession) {
+  // Se o usuário quiser salvar a sessão, salva no localStorage
     if(saveSession) {
         localStorage.setItem("session", data);
     }
@@ -85,12 +117,18 @@ function saveSession(data, saveSession) {
 
 }
 
+// ============================
+// PEGA A CONTA
+// ============================
 function getAccount(key) {
+  // Pega os dados da conta no localStorage
     const account = localStorage.getItem(key);
 
+    // Se a conta existir, converte de volta para objeto
     if(account) {
         return JSON.parse(account);
     }
 
+    // Se não existir, retorna string vazia
     return "";
 }
